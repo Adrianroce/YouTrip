7d8ac1b43cbc536db0e85bf3d0a0ffaa8078df43
Adrian Roman <adrianrc999@gmail.com>
2023-04-05 12:01:29 +0200

    first commit

---
+++ Documentos/0001.sql
@@ -0,0 +1,61 @@
+INSERT INTO "Gestion_ciudad" (nombre, codigo, familia, gastronomia, cultura, transporte, pareja, alojamiento, ocio, fiesta, mejor_epoca) VALUES
+('Bangkok', 'BKG', 4, 5, 4, 4, 4, 3, 5, 4, '11, 1'),
+('París', 'FRA', 5, 5, 5, 4, 5, 4, 4, 4, '3, 5 | 10, 11'),
+('Londres', 'GBR', 4, 4, 5, 5, 4, 3, 5, 3, '3, 5 | 9, 11'),
+('Dubái', 'UAE', 3, 4, 3, 4, 3, 4, 5, 5, '11, 2'),
+('Singapur', 'SGP', 4, 5, 4, 5, 4, 4, 4, 3, '2, 4 | 7, 8'),
+('Kuala Lumpur', 'MYS', 4, 4, 3, 4, 3, 4, 3, 3, '5, 7 | 12, 2'),
+('Nueva York', 'USA', 4, 5, 5, 5, 5, 3, 5, 5, '4, 6 | 9, 11'),
+('Estambul', 'TUR', 4, 4, 5, 4, 4, 4, 4, 4, '4, 5 | 9, 11'),
+('Tokio', 'JPN', 4, 5, 5, 5, 5, 3, 4, 3, '3, 5 | 9, 11'),
+('Antalya', 'TRA', 5, 4, 3, 4, 5, 4, 5, 3, '4, 5 | 9, 11'),
+('Phuket', 'PHU', 5, 4, 3, 4, 5, 4, 4, 4, '11, 2'),
+('Mecca', 'SAU', 2, 2, 5, 2, 2, 2, 2, 2, '12, 5'),
+('Roma', 'ITA', 5, 5, 5, 4, 5, 3, 5, 3, '4, 6 | 9, 11'),
+('Pattaya', 'TTA', 5, 4, 3, 4, 5, 4, 5, 5, '11, 2'),
+('Shenzhen', 'CHN', 3, 3, 2, 4, 3, 4, 2, 2, '10, 12 | 3, 5');
+
+INSERT INTO "Gestion_ciudad" (codigo, nombre, familia, gastronomia, cultura, transporte, pareja, alojamiento, ocio, fiesta, mejor_epoca)
+VALUES 
+('BCN', 'Barcelona', 4, 5, 5, 4, 5, 3, 5, 4, '3, 5 | 10, 11'),
+('BAL', 'Bali', 5, 4, 4, 3, 5, 4, 4, 3, '4, 10'),
+('MIL', 'Milán', 3, 5, 4, 4, 3, 3, 3, 3, '3, 5 | 10, 11'),
+('OSA', 'Osaka', 5, 4, 5, 4, 5, 4, 4, 4, '3, 5 | 10, 11'),
+('PMI', 'Palma de Mallorca', 4, 4, 4, 4, 4, 4, 4, 3, '5, 10'),
+('TPE', 'Taipéi', 4, 4, 5, 4, 5, 4, 4, 3, '3, 5 | 9, 11'),
+('CUN', 'Cancún', 5, 4, 3, 4, 5, 4, 5, 5, '11, 4'),
+('PRA', 'Praga', 4, 4, 5, 4, 5, 4, 4, 3, '4, 6 | 9, 10'),
+('VIE', 'Viena', 4, 4, 5, 4, 5, 4, 4, 3, '4, 5 | 9, 10'),
+('HKG', 'Hong Kong', 4, 5, 4, 5, 4, 4, 5, 5, '10, 12'),
+('JHB', 'Johor Bahru', 4, 3, 2, 3, 3, 3, 3, 2, '6, 8'),
+('MIA', 'Miami', 4, 4, 3, 4, 5, 4, 5, 5, '11, 4'),
+('PNH', 'Phnom Penh', 3, 3, 4, 3, 3, 3, 4, 2, '11, 2'),
+('SEL', 'Seúl', 4, 4, 5, 4, 5, 4, 4, 4, '3, 5 | 9, 11'),
+('AGA', 'Agra', 3, 3, 5, 3, 3, 3, 2, 1, '11, 3');
+
+INSERT INTO "Gestion_ciudad" (nombre, codigo, familia, gastronomia, cultura, transporte, pareja, alojamiento, ocio, fiesta, mejor_epoca)
+VALUES
+('Mumbai', 'IND001', 4, 5, 5, 3, 5, 3, 5, 4, '11, 2'),
+('Barcelona', 'ESP001', 5, 5, 5, 5, 5, 4, 5, 5, '4, 6 | 9, 11'),
+('Orlando', 'USA001', 5, 4, 4, 4, 5, 4, 5, 4, '1, 4 | 9, 12'),
+('Sídney', 'AUS001', 5, 4, 4, 4, 5, 4, 5, 4, '9, 11 | 3, 5'),
+('Zhengzhou', 'CHN001', 3, 3, 4, 4, 3, 3, 3, 2, '5, 10'),
+('Granada', 'ESP002', 5, 4, 5, 4, 5, 4, 5, 3, '4, 6 | 9, 11'),
+('Beijing', 'CHN002', 4, 5, 5, 4, 4, 4, 5, 3, '9, 11 | 3, 5'),
+('Abu Dhabi', 'UAE001', 4, 4, 4, 4, 4, 4, 4, 3, '11, 3'),
+('Hangzhou', 'CHN003', 4, 4, 5, 4, 4, 4, 5, 2, '3, 5 | 9, 11'),
+('San Francisco', 'USA002', 5, 5, 5, 4, 5, 3, 5, 3, '9, 11 | 3, 5'),
+('Florencia', 'ITA001', 4, 5, 5, 4, 4, 4, 5, 3, '4, 6 | 9, 11'),
+('Xi''an', 'CHN004', 4, 4, 5, 4, 4, 4, 4, 2, '3, 5 | 9, 11'),
+('Los Ángeles', 'USA003', 5, 4, 5, 4, 5, 4, 5, 4, '9, 11 | 3, 5'),
+('Moscú', 'RUS001', 4, 4, 5, 4, 4, 4, 4, 3, '5, 9'),
+('Siem Reap', 'CAM001', 4, 3, 5, 3, 3, 3, 3, 2, '11, 3'),
+('Madrid', 'ESP003', 5, 5, 5, 4, 5, 4, 5, 4, '4, 6 | 9, 11');
+
+INSERT INTO "Gestion_ciudad" (nombre, codigo, familia, gastronomia, cultura, transporte, pareja, alojamiento, ocio, fiesta, mejor_epoca) 
+VALUES 
+('Nueva Delhi', 'NDL', 4, 5, 5, 4, 5, 2, 5, 4, '10, 3'),
+('Chiang Mai', 'CHM', 5, 4, 4, 3, 5, 3, 5, 3, '11, 2'),
+('Chicago', 'CHI', 5, 4, 4, 5, 5, 3, 4, 4, '4, 6 | 9, 11'),
+('Budapest', 'BUD', 4, 5, 5, 4, 5, 3, 4, 4, '4, 5 | 9, 10');
+
